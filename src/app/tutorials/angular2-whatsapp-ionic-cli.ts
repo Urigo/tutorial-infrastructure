2c50247a6ebfef9bfed3e66d61b54fcb5ac19e59
Dotan Simha <dotansimha@Dotans-MacBook-Pro.local>
2016-11-22 23:37:26 +0200

    added whatsapp 2 with ionic cli from remote using Tortilla

--- src/app/tutorials/angular2-whatsapp-ionic-cli.ts
+++ src/app/tutorials/angular2-whatsapp-ionic-cli.ts
@@ -1,51 +1,53 @@
 import {TutorialDefinition} from '../core/tutorial-definition';
 
+const markdownUrlBase = 'https://raw.githubusercontent.com/Urigo/Ionic2CLI-Meteor-WhatsApp/master/manuals/';
+
 export const ANGULAR2_WHATSAPP_IONIC_CLI: TutorialDefinition = {
   id: 'whatsapp2-ionic-tutorial',
   name: 'WhatsApp clone with Angular 2 and Ionic CLI',
-  gitHub: 'DAB0mB/ionic2-meteor-messenger',
-  patchFile: '/assets/patches/angular2-whatsapp-ionic-cli.multi.patch',
+  gitHub: 'Urigo/Ionic2CLI-Meteor-WhatsApp',
+  patchFile: markdownUrlBase + 'whatsapp2-ionic-cli.multi.patch',
   baseRoute: 'ionic',
   steps: [
     {
       url: '/setup',
       name: 'Initial setup',
-      template: '/assets/templates/angular2-whatsapp-ionic-cli/tutorials.whatsapp2.ionic.step_01.md'
+      template: markdownUrlBase + 'templates/step1.md',
     },
     {
       url: '/chats-page',
       name: 'Chats page',
-      template: '/assets/templates/angular2-whatsapp-ionic-cli/tutorials.whatsapp2.ionic.step_02.md'
+      template: markdownUrlBase + 'templates/step2.md',
     },
     {
       url: '/meteor-server-side',
       name: 'Realtime Meteor Server',
-      template: '/assets/templates/angular2-whatsapp-ionic-cli/tutorials.whatsapp2.ionic.step_03.md'
+      template: markdownUrlBase + 'templates/step3.md',
     },
     {
       url: '/messages-page',
       name: 'Messages Page',
-      template: '/assets/templates/angular2-whatsapp-ionic-cli/tutorials.whatsapp2.ionic.step_04.md'
+      template: markdownUrlBase + 'templates/step4.md',
     },
     {
       url: '/authentication',
       name: 'Authentication',
-      template: '/assets/templates/angular2-whatsapp-ionic-cli/tutorials.whatsapp2.ionic.step_05.md'
+      template: markdownUrlBase + 'templates/step5.md',
     },
     {
       url: '/chats-mutations',
       name: 'Chats Creation & Removal',
-      template: '/assets/templates/angular2-whatsapp-ionic-cli/tutorials.whatsapp2.ionic.step_06.md'
+      template: markdownUrlBase + 'templates/step6.md',
     },
     {
       url: '/privacy',
       name: 'Privacy & Security',
-      template: '/assets/templates/angular2-whatsapp-ionic-cli/tutorials.whatsapp2.ionic.step_07.md'
+      template: markdownUrlBase + 'templates/step7.md',
     },
     {
       url: '/summary',
       name: 'Summary',
-      template: '/assets/templates/angular2-whatsapp-ionic-cli/tutorials.whatsapp2.ionic.step_08.md'
+      template: markdownUrlBase + 'templates/step8.md',
     }
   ]
 };
