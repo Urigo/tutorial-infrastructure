c841c32f69c055d9f482b020a41dfb2434857387
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-10-13 17:29:22 +0200

    Export CurrentGitTagRevision and bump version to 0.1.19

--- src/current-tutorial.ts
+++ src/current-tutorial.ts
@@ -5,11 +5,13 @@ export class ActivatedTutorial {
   public tutorial: Subject<TutorialDefinition>;
   public step: Subject<TutorialStep>;
   public steps: Subject<TutorialStep[]>;
+  public gitTagRevision: Subject<string>;
 
   constructor() {
     this.step = new Subject<TutorialStep>();
     this.tutorial = new Subject<TutorialDefinition>();
     this.steps = new Subject<TutorialStep[]>();
+    this.gitTagRevision = new Subject<string>();
   }
 
   updateCurrentTutorial(tutorial: TutorialDefinition) {
@@ -22,5 +24,9 @@ export class ActivatedTutorial {
 
   updateCurrentSteps(steps: TutorialStep[]) {
     this.steps.next(steps);
+  }
+
+  updateCurrentGitTagRevision(gitTagRevision: string) {
+      this.gitTagRevision.next(gitTagRevision);
   }
 }
